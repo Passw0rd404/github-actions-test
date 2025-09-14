51d961edabd3449bb0ba13cd90cbf5aed21f2235
Pa$$w0rd <a26263842z@gmail.com>
2025-09-14 10:45:05 +0300

    new js app

---
+++ src/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#src/test.sh
+EXPECTED="hello, test!"
+OUTPUT=$(node —e "console.log( require( ' ./src/app' ) ( 'test' ))")
+if [ "$OUTPUT" == "$EXPECTED" ]; then
+
+echo "Test passed!"
+
+exit 0
+
+else
+
+echo "Test failed! '$EXPECTED' is not '$OUTPUT'"
+
+exit 1
+
+fi
